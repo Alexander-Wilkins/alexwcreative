c65f8eb2eefde2a7d9eb643ead60d7474ebb82f3
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-28 12:08:26 -0700

    testing github actions

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/alexwcreative",
+  distDir: "dist",
   output: "export", // This will generate static HTML files
   reactStrictMode: true,
 };
