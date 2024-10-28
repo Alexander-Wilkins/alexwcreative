857c9a217c60fb2b5aa98f1d4d1dee33a68e1533
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-28 16:06:15 -0700

    use gregrickaby/nextjs-github-pages

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,11 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   basePath: isProd ? "/alexwcreative" : "",
-  distDir: "dist",
   output: "export", // This will generate static HTML files
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
