a2d2b673cf1c7524e72b09f333cf17525b714e66
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-28 14:51:32 -0700

    hotfix: svgr bug

--- next.config.ts
+++ next.config.ts
@@ -3,24 +3,14 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/alexwcreative": "",
+  basePath: isProd ? "/alexwcreative" : "",
   distDir: "dist",
   output: "export", // This will generate static HTML files
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [
-        {
-          loader: '@svgr/webpack',
-          options: {
-            prettier: false,
-            svgo: true,
-            svgoConfig: { plugins: [{ removeViewBox: false }] },
-            titleProp: true,
-          },
-        },
-      ],
+      use: ["@svgr/webpack"],
     });
 
     return config;
