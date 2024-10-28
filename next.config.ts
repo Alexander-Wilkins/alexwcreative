bf58402bb16e338ed3bb778e7abe60a252903f7a
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-28 14:03:07 -0700

    svg test anyone??????

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
 import type { NextConfig } from "next";
+import svgr from "@svgr/webpack";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   basePath: "/alexwcreative",
   distDir: "dist",
   output: "export", // This will generate static HTML files
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
