5fad845c89482400788eaecc4b30dc50a3e0eeb0
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-28 16:42:39 -0700

    Add blue sky background!

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import BlueSkyBackground from "@/components/BlueSkyBackground";
-import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Alex Wilkins - Web Developer",
--- src/components/BlueSkyBackground.tsx
+++ src/components/BlueSkyBackground.tsx
@@ -1,8 +1,18 @@
-import Image from 'next/image'
-import blueSkyBg from '@/assets/imgs/background-clouds.png';
+import Image from "next/image";
+import blueSkyBg from "@/assets/imgs/background-clouds.png";
 
 export default function BlueSkyBackground() {
   return (
-    <Image src={blueSkyBg}  alt="blue open sky with clounds" />
-  )
+    <Image
+      src={blueSkyBg}
+      alt="blue open sky with clounds"
+      placeholder="blur"
+      quality={100}
+      fill
+      sizes="100vw"
+      style={{
+        objectFit: "cover",
+      }}
+    />
+  );
 }
