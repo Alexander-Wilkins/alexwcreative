3c7f849a8fffe9772464ab24024e8d7e0c853b46
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-29 13:46:23 -0700

    draggable window

--- src/components/Window.tsx
+++ src/components/Window.tsx
@@ -1,11 +1,32 @@
-import { ReactNode } from 'react';
+"use client";
+
+import { ReactNode, useRef } from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 interface WindowProps {
     children: ReactNode;
+    size: string;
 }
 
-export default function Window({ children }: WindowProps) {
+export default function Window({ children, size }: WindowProps) {
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const nodeRef = useRef(null);
+
+    const handleDrag = (_e: unknown, ui: { deltaX: number; deltaY: number; }) => {
+        const { x, y } = position;
+        setPosition({ x: x + ui.deltaX, y: y + ui.deltaY });
+    };
+
     return (
-        <div id='window-container' className='rounded-xl bg-[#0038FF] border-solid border-2 border-xp-blue'>{children}</div>
+        <Draggable nodeRef={nodeRef} position={position} onDrag={handleDrag}>
+            <div
+                ref={nodeRef}
+                id="window-container"
+                className={`rounded-xl bg-[#0038FF] border-solid border-2 border-xp-blue ${size}`}
+            >
+                {children}
+            </div>
+        </Draggable>
     );
 }
