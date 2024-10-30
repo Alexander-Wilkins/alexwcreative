ce2ab874aa3bb498fe8bf04088444ae4ec15da66
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-10-30 14:30:50 -0700

    mobile version for color palettes

--- src/components/ColorPalette.tsx
+++ src/components/ColorPalette.tsx
@@ -38,25 +38,25 @@ export default function ColorPalette() {
       <div className="flex flex-row gap-2 bg-ms-bronze-ui p-2">
         <div
           id="tan-box"
-          className="h-16 w-16 border-[1px] border-solid border-ms-bronze-bg-ui border-b-white border-r-white"
+          className="h-14 w-14 border-[1px] border-solid border-ms-bronze-bg-ui border-b-white border-r-white sm:h-16 sm:w-16"
         >
           <div
             id="primary-color"
-            className={`relative left-2 top-2 z-10 h-[1.75rem] w-[1.75em] border-[1px] border-b-white border-l-black border-r-white border-t-black bg-black`}
+            className={`relative left-2 top-2 z-10 h-[1.5rem] w-[1.5rem] border-[1px] border-b-white border-l-black border-r-white border-t-black bg-black sm:h-[1.75rem] sm:w-[1.75em]`}
           ></div>
           <div
             id="secondary-color"
-            className={`relative -top-1 left-6 h-[1.75rem] w-[1.75em] border-[1px] border-b-white border-l-black border-r-white border-t-black bg-white`}
+            className={`relative left-5 h-[1.5rem] w-[1.5rem] border-[1px] border-b-white border-l-black border-r-white border-t-black bg-white sm:-top-1 sm:left-6 sm:h-[1.75rem] sm:w-[1.75em]`}
           ></div>
         </div>
         <div
           id="colors-container"
-          className="grid auto-cols-auto grid-flow-col grid-rows-2 gap-1"
+          className="grid auto-cols-auto grid-flow-col grid-rows-3 gap-1"
         >
           {Object.entries(colors).map(([colorName, colorValue]) => (
             <div
               key={colorName}
-              className={`h-[1.75rem] w-[1.75em] border-[1px] border-b-white border-l-black border-r-white border-t-black`}
+              className={`h-[1.25rem] w-[1.25rem] border-[1px] border-b-white border-l-black border-r-white border-t-black sm:h-[1.75rem] sm:w-[1.75em]`}
               style={{ backgroundColor: colorValue }}
             ></div>
           ))}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,14 @@ export default function Navbar() {
   return (
     <div>
       <nav className="bg-ms-bronze-ui">
+        {/* TODO: Make a hamburger menu with all of the navbar items */}
         <ul className={`${vt323.className} text-black text-2xl flex flex-row`}>
           <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">File</li>
-          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">Edit</li>
-          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">View</li>
-          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer hidden sm:block">Image</li>
-          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer hidden sm:block">Colors</li>
-          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">Help</li>
+          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">About Me</li>
+          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">Projects</li>
+          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer hidden sm:block">Web Comic</li>
+          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer hidden sm:block">Illustration Portfolio</li>
+          <li className="px-4 hover:bg-ms-light-gray hover:text-white cursor-pointer">Blog</li>
         </ul>
       </nav>
     </div>
