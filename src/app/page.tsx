fdf773e744d914e3af2ef6f11e48830ab9a46cb3
alexwilkins-berwick <159204049+alexwilkins-berwick@users.noreply.github.com>
2024-11-01 16:51:15 -0700

    scrolling within the canvas area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,21 @@ import ColorPalette from "@/components/ColorPalette";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden w-screen h-[99vh]">
+    <div className="h-[99vh] w-screen overflow-hidden">
       {/* TODO: Make this an MSPaint Component, so that "Windows" are their own thing */}
       <Window size="w-[23rem] sm:w-[67rem]" title="alexwcreative.com">
         <Navbar />
         <main className="relative grid h-full w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] bg-ms-bronze-bg-ui">
           <div
             id="left-container"
-            className="sm:w-[5.5rem] w-[5.25rem] border-[1px] border-ms-bronze-bg-ui bg-ms-bronze-ui pl-[0.65rem]"
+            className="w-[5.25rem] border-[1px] border-ms-bronze-bg-ui bg-ms-bronze-ui pl-[0.65rem] sm:w-[5.5rem]"
           >
             <Tools />
           </div>
-          <div id="right-container" className="overflow-hidden">
-            <Canvas />
+          <div id="right-container" className="h-full w-full overflow-hidden">
+            <div className="overflow-auto max-h-[40.25rem]">
+              <Canvas />
+            </div>
           </div>
           <div id="bottom-container" className="col-span-2">
             <ColorPalette />
--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,13 +4,63 @@ import mainDrawing from "@/assets/imgs/main-page.png";
 export default function Canvas() {
   return (
     <div>
-    <div id="drawing-canvas" className="bg-white mt-2 ml-2 sm:mt-4 sm:ml-4 mb-20 inline-block p-4 sm:p-10" style={{ position: 'relative' }}>
-        <div style={{ position: 'absolute', top: -4, left: -4, width: '4px', height: '4px', backgroundColor: '#f5efef', border: '1px solid #8498df' }}></div>
-        <div style={{ position: 'absolute', top: -4, right: -4, width: '4px', height: '4px', backgroundColor: '#f5efef', border: '1px solid #8498df' }}></div>
-        <div style={{ position: 'absolute', bottom: -4, left: -4, width: '4px', height: '4px', backgroundColor: '#f5efef', border: '1px solid #8498df' }}></div>
-        <div style={{ position: 'absolute', bottom: -4, right: -4, width: '4px', height: '4px', backgroundColor: '#f5efef', border: '1px solid #8498df' }}></div>
-        <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-    </div>
+      <div
+        id="drawing-canvas"
+        className="mb-20 ml-2 mt-2 inline-block bg-white p-4 sm:ml-4 sm:mt-4 sm:p-10"
+        style={{ position: "relative" }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: -4,
+            left: -4,
+            width: "4px",
+            height: "4px",
+            backgroundColor: "#f5efef",
+            border: "1px solid #8498df",
+          }}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            top: -4,
+            right: -4,
+            width: "4px",
+            height: "4px",
+            backgroundColor: "#f5efef",
+            border: "1px solid #8498df",
+          }}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: -4,
+            left: -4,
+            width: "4px",
+            height: "4px",
+            backgroundColor: "#f5efef",
+            border: "1px solid #8498df",
+          }}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            bottom: -4,
+            right: -4,
+            width: "4px",
+            height: "4px",
+            backgroundColor: "#f5efef",
+            border: "1px solid #8498df",
+          }}
+        ></div>
+        
+          <Image src={mainDrawing} alt="Test Drawing" quality={100} />
+          <Image src={mainDrawing} alt="Test Drawing" quality={100} />
+          <Image src={mainDrawing} alt="Test Drawing" quality={100} />
+          <Image src={mainDrawing} alt="Test Drawing" quality={100} />
+          <Image src={mainDrawing} alt="Test Drawing" quality={100} />
+        
+      </div>
     </div>
   );
 }
