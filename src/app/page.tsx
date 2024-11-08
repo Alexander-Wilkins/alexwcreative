f2d5db390ccdb3b65316da692bd365901421ad2f
alexanderwilkins <alvanwil@gmail.com>
2024-11-08 15:04:18 -0700

    prep for next/font hotfix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,6 @@ export default function Home() {
     <div className="">
       {/* TODO: Make this an MSPaint Component, so that "Windows" are their own thing */}
       <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-      <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-      <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-      <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-      <Image src={mainDrawing} alt="Test Drawing" quality={100} />
-      {/* <Window
-        size="w-[23rem] sm:w-[67rem]"
-        defaultPos="ml-8 sm:ml-[23.125rem] sm:mt-10"
-        title="alexwcreative.com"
-      >
-        <Navbar />
-        <main className="relative grid h-full w-full grid-cols-[auto_1fr] grid-rows-[auto_1fr_auto] bg-ms-bronze-bg-ui">
-          <div
-            id="left-container"
-            className="w-[5.25rem] border-[1px] border-ms-bronze-bg-ui bg-ms-bronze-ui pl-[0.65rem] sm:w-[5.5rem]"
-          >
-            <Tools />
-          </div>
-          <div id="right-container" className="h-full w-full overflow-hidden">
-            <div className="max-h-[40.25rem] overflow-auto">
-              <Canvas />
-            </div>
-          </div>
-          <div id="bottom-container" className="col-span-2">
-            <ColorPalette />
-          </div>
-        </main>
-      </Window> */}
     </div>
   );
 }
